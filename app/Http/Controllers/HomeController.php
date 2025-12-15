6b7fc9b70c13872ac2ac7bb21d9ab7cc861d9c46
jiuugo <hugosv2003@gmail.com>
2025-12-15 06:00:23 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class HomeController
 {
     public function index(){
+        $data = [
+            'legoArticles' => Brand::find(1)->articles
+        ];
         return view('home');
     }
 }
